b963c5f5931121274f9e86266aeccaba72a86d44
Aleksandr Gordienko <54080214+reclaite@users.noreply.github.com>
2021-03-17 21:41:59 +0300

    Изменены имена коллекций

--- CoolNumber/src/CoolNumber.java
+++ CoolNumber/src/CoolNumber.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class CoolNumber {
 
-    private ArrayList<String> NUMBERS = new ArrayList<>();
-    private HashSet<String> HASHNUMBERS = new HashSet<>();
-    private TreeSet<String> TREENUMBERS = new TreeSet<>();
+    private ArrayList<String> numbers = new ArrayList<>();
+    private HashSet<String> hashNumbers = new HashSet<>();
+    private TreeSet<String> treeNumbers = new TreeSet<>();
 
     public void generateCoolNumbers() {
         String[] alphabet = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
@@ -16,13 +16,13 @@ public class CoolNumber {
                 //первые 2 нуля
                 for (int j = 1; j <= 10; j++) {
                     System.out.println(new Formatter().format("%s%03d%s%02d\n", s, j, s + s, l).toString().trim());
-                    NUMBERS.add(new Formatter().format("%s%03d%s%02d\n", s, j, s + s, l).toString().trim());
+                    numbers.add(new Formatter().format("%s%03d%s%02d\n", s, j, s + s, l).toString().trim());
                 }
 
                 //буквы и цифры
                 for (int j = 111; j <= 999; j += 111) {
                     System.out.println(new Formatter().format("%s%03d%s%02d\n", s, j, s + s, l).toString().trim());
-                    NUMBERS.add(new Formatter().format("%s%03d%s%02d\n", s, j, s + s, l).toString().trim());
+                    numbers.add(new Formatter().format("%s%03d%s%02d\n", s, j, s + s, l).toString().trim());
                 }
 
                 //одинаковые цифры
@@ -31,7 +31,7 @@ public class CoolNumber {
                         for (String item : alphabet) {
                             for (String element : alphabet) {
                                 System.out.println(new Formatter().format("%s%03d%s%02d\n", value, i, item + element, l).toString().trim());
-                                NUMBERS.add(new Formatter().format("%s%03d%s%02d\n", value, i, item + element, l).toString().trim());
+                                numbers.add(new Formatter().format("%s%03d%s%02d\n", value, i, item + element, l).toString().trim());
                             }
                         }
                     }
@@ -39,19 +39,19 @@ public class CoolNumber {
             }
 
         }
-        HASHNUMBERS.addAll(NUMBERS);
-        TREENUMBERS.addAll(NUMBERS);
+        hashNumbers.addAll(numbers);
+        treeNumbers.addAll(numbers);
 
         Date completeDate = new Date();
         System.out.println("Все номера сгенерированы!");
         System.out.println("Процесс занял " + (completeDate.getTime() - startDate.getTime()) + " мс");
-        System.out.println("Зарегистрировано " + NUMBERS.size() + " комбинаций номеров");
+        System.out.println("Зарегистрировано " + numbers.size() + " комбинаций номеров");
     }
 
     public void bruteForce(String sign) {
         Date startDate = new Date();
         boolean isFound = false;
-        for (String number : NUMBERS) {
+        for (String number : numbers) {
             if (number.equalsIgnoreCase(sign)) {
                 isFound = true;
                 break;
@@ -64,7 +64,7 @@ public class CoolNumber {
     public void hashSetSearch(String sign) {
         Date startDate = new Date();
         boolean isFound = false;
-        if (HASHNUMBERS.contains(sign)) {
+        if (hashNumbers.contains(sign)) {
             isFound = true;
         }
         Date completeDate = new Date();
@@ -74,7 +74,7 @@ public class CoolNumber {
     public void treeSetSearch(String sign) {
         Date startDate = new Date();
         boolean isFound = false;
-        if (TREENUMBERS.contains(sign)) {
+        if (treeNumbers.contains(sign)) {
             isFound = true;
         }
         Date completeDate = new Date();
