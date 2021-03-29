2f44f6fa3a6cb0232a8948455889b788cb2787c5
Alexander Gordienko <defiantlore@gmail.com>
2021-03-29 17:30:18 +0300

    Сортировка по последнему символу
    Реализована сортировка строк по последнему символу через stream

--- StringWorker/src/ru/kubsu/main/Main.java
+++ StringWorker/src/ru/kubsu/main/Main.java
@@ -1,15 +1,18 @@
 package ru.kubsu.main;
 
-import ru.kubsu.main.util.DateHandler;
+import ru.kubsu.main.util.StringHandler;
 
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        DateHandler handler = new DateHandler();
-        Scanner scanner = new Scanner(System.in);
-        String string = scanner.next();
-        handler.convertBirthday(string);
+        StringHandler handler = new StringHandler();
+        Scanner scan = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < 5; i++) {
+            list.add(scan.nextLine());
+        }
+        handler.sortByLastChar(list.toArray(new String[0]));
     }
 }
--- StringWorker/src/ru/kubsu/main/util/StringHandler.java
+++ StringWorker/src/ru/kubsu/main/util/StringHandler.java
@@ -1,6 +1,12 @@
 package ru.kubsu.main.util;
 
+import java.util.*;
+
+import static java.util.Map.Entry.comparingByValue;
+
 public class StringHandler {
+
+    private String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
     public void reverseString(String string) {
         char[] chars = string.toCharArray();
@@ -9,5 +15,17 @@ public class StringHandler {
             returnStr += chars[i];
         }
         System.out.println(returnStr);
+    }
+
+    public void sortByLastChar(String[] args) {
+        HashMap<String, Character> hash = new HashMap<>();
+        ArrayList<String> sortedList = new ArrayList<>();
+        Arrays.stream(args).forEach(arg -> {
+            char[] list = arg.toCharArray();
+            hash.put(arg, list[list.length - 1]);
+        });
+        hash.entrySet().stream().sorted(comparingByValue()).forEach(arg -> sortedList.add(arg.getKey()));
+        System.out.println("Отсортированный список: ");
+        for(String arg : sortedList) System.out.println(arg);
     }
 }
